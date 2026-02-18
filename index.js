eb047f7e7499dca613a84313cdc6b7fac1fcac5a
nidvjj-sudo <nidvjj@gmail.com>
2026-02-19 06:12:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const client = new Discord.Client({
 const keepAlive = require('./server.js');
 keepAlive();
 
+namebutton = "Join Discord"
+link = "https://discord.gg/Ta63JWwbvD" 
 const png1 = "https://cdn.discordapp.com/attachments/1464785403503448222/1473451993014992917/fc4bc0c2d69dd94b155bfc96b7a6e6ed.gif?ex=6996429a&is=6994f11a&hm=6baa306dc84bfda1b74c33ed839dc0ec65cdf5db410961593d75fc2eaa09abc9&";
 const png2 = "https://cdn.discordapp.com/attachments/1464785403503448222/1473452143947026669/7223788ac37a98ed63d8396405ce7187.jpg?ex=699642be&is=6994f13e&hm=8eb9bf0c7e8870bc38b183b4ebb734f49151b3003486a9206d67859b3f661d75&";
 
@@ -26,7 +28,8 @@ client.on("ready", async () => {
       .setDetails(`รับเติมไนโต กรอบ เกม`)
       .setStartTimestamp(Date.now())
       .setAssetsLargeImage(png1)
-      .setAssetsSmallImage(png2);
+      .setAssetsSmallImage(png2)
+      .addButton(namebutton, link);
 
     client.user.setActivity(d);
   }, 500);
